dbd04c285e1de6e85341ae8901b1036764fdf528
Jose D <joserafael161@hotmail.com>
2020-01-06 11:28:55 -0500

    Add navigation bar

--- src/ui/header/Header.tsx
+++ src/ui/header/Header.tsx
@@ -3,9 +3,16 @@ import './Header.css';
 
 const Header: React.FC = () => {
   return (
-      <header className="header">
-          It works
-      </header>
+    <header className="header">
+      <a className="header__logo" href="/">
+        <img className="header__logo-image" src="/images/netflix.png" alt="Netflix"></img>
+      </a>
+      <ul className="header__nav">
+        <li className="nav__option"><a className="nav__option-anchor nav__option-anchor--selected" href="/xd">Inicio</a></li>
+        <li className="nav__option"><a className="nav__option-anchor" href="/">Programas</a></li>
+        <li className="nav__option"><a className="nav__option-anchor" href="/">Películas</a></li>
+      </ul>
+    </header>
   );
 }
 
