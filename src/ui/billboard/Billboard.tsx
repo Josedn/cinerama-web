82b16febc9baacba780f41540b5c640bd3c99277
Jose D <joserafael161@hotmail.com>
2020-02-05 19:02:21 -0500

    Add search bar

--- src/ui/billboard/Billboard.tsx
+++ src/ui/billboard/Billboard.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './Billboard.scss';
 
+const images = ["http://image.tmdb.org/t/p/w500/cSKXdkm38HIMsZZNYJDIOtfOV9B.jpg", "http://image.tmdb.org/t/p/w500/7IiTTgloJzvGI1TAYymCfbfl3vT.jpg", "http://image.tmdb.org/t/p/w500/l5lIlibDxnAK30aa28p0UtRldi.jpg", "http://image.tmdb.org/t/p/w500/vqzNJRH4YyquRiWxCCOH0aXggHI.jpg", "http://image.tmdb.org/t/p/w500/9AICdnpFIkZOx3OHz0fj3jwJHdK.jpg", "http://image.tmdb.org/t/p/w500/4AHkWCSau4OmMJTFo36bFMlCRtu.jpg"];
+
 const Movie = () => {
+  const randomImg = images[Math.floor(images.length * Math.random())];
   return (
     <div className="movie">
-      <img className="movie__preview" src="http://image.tmdb.org/t/p/w500/7IiTTgloJzvGI1TAYymCfbfl3vT.jpg" alt="My sample" />
+      <img className="movie__preview" src={randomImg} alt="My sample" />
       <div className="movie__info">
         <h4 className="movie__title">The Walking Dead</h4>
         <h5 className="movie__year">2007</h5>
@@ -18,7 +21,7 @@ const Group = () => {
     <div className="billboard__group">
       <h2 className="group__title">Populares en Netflix</h2>
       <div className="group__list">
-        <Movie /><Movie /><Movie /><Movie /><Movie /><Movie /><Movie /><Movie /><Movie /><Movie /><Movie /><Movie />
+        <Movie /><Movie /><Movie /><Movie /><Movie /><Movie /><Movie /><Movie /><Movie /><Movie />
       </div>
     </div>
   );
--- src/ui/header/Header.tsx
+++ src/ui/header/Header.tsx
@@ -8,9 +8,19 @@ const Header: React.FC = () => {
         <img className="header__logo-image" src="/images/netflix.png" alt="Netflix"></img>
       </a>
       <ul className="header__nav">
-        <li className="nav__option"><a className="nav__option-anchor nav__option-anchor--selected" href="/xd">Inicio</a></li>
+        <li className="nav__option"><a className="nav__option-anchor nav__option-anchor--selected" href="/">Inicio</a></li>
         <li className="nav__option"><a className="nav__option-anchor" href="/">Programas</a></li>
         <li className="nav__option"><a className="nav__option-anchor" href="/">Películas</a></li>
+      </ul>
+      <ul className="header__nav header__nav--options">
+        <li className="nav__option"><a className="nav__option-anchor" href="/">Salir</a></li>
+        <li className="nav__option">
+          <div className="nav__search">
+            <i className="nav__search-icon-glass fa fa-search"></i>
+            <input className="nav__search-input" type="text" placeholder="Títulos, personas, géneros" />
+            <i className="nav__search-icon-close fa fa-close"></i>
+          </div>
+        </li>
       </ul>
     </header>
   );
