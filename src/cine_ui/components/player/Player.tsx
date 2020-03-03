2a113b7aa72b67b98c6384d6ebe58c7d3877e242
Jose D <joserafael161@hotmail.com>
2020-03-02 23:55:01 -0500

    Add basic player buttons

--- src/cine_ui/components/player/Player.tsx
+++ src/cine_ui/components/player/Player.tsx
@@ -8,12 +8,36 @@ class Player extends React.Component<PlayerProps, PlayerState> {
     render() {
         return (
             <div className="player">
-                <video className="player__video" controls>
+                <video className="player__video">
                     <source src="/media/sample_video.mp4" type="video/mp4" />
                     Your browser does not support HTML5 video.
                 </video>
-                <div className="player__info"></div>
-                <div className="player__control_bar"></div>
+                <div className="player_controlzone">
+                    <div className="player_backdrop--active player_backdrop-top"></div>
+                    <div className="player_backdrop--active player_backdrop-bottom"></div>
+                    <div className="player__control_bottom">
+                        <div className="player__control-progress">
+
+                        </div>
+                        <div className="player__control-buttons">
+                            <button className="player__control-button">
+                                <i className="fa fa-play player__control-button-icon" aria-hidden="true"></i>
+                            </button>
+                            <button className="player__control-button">
+                                <i className="fa fa-backward player__control-button-icon" aria-hidden="true"></i>
+                            </button>
+                            <button className="player__control-button">
+                                <i className="fa fa-forward player__control-button-icon" aria-hidden="true"></i>
+                            </button>
+                            <button className="player__control-button">
+                                <i className="fa fa-volume-up player__control-button-icon" aria-hidden="true"></i>
+                            </button>
+                            <div className="player__control-title">
+                                La La Land
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
