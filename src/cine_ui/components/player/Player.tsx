0250dfdc1c7a8c8945382171802bc78404be01d6
Jose D <joserafael161@hotmail.com>
2020-03-04 00:23:31 -0500

    Add progress bar

--- src/cine_ui/components/player/Player.tsx
+++ src/cine_ui/components/player/Player.tsx
@@ -15,13 +15,35 @@ class Player extends React.Component<PlayerProps, PlayerState> {
                 <div className="player_controlzone">
                     <div className="player_backdrop--active player_backdrop-top"></div>
                     <div className="player_backdrop--active player_backdrop-bottom"></div>
+                    <div className="player__control_top">
+                        <div className="player__control-buttons">
+                            <button className="player__control-button">
+                                <i className="fa fa-angle-left player__control-button-icon" aria-hidden="true"></i>
+                            </button>
+                        </div>
+                    </div>
                     <div className="player__control_bottom">
-                        <div className="player__control-progress">
-
+                        <div className="player__control-progress-container">
+                            <div className="player__control-progress-bar">
+                                <div className="scrubber-container">
+                                    <div className="scrubber-bar">
+                                        <div className="scrubber__track">
+                                            <div className="scrubber__buffered"></div>
+                                            <div className="scrubber__progress"></div>
+                                            <div className="scrubber__play_head"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="player__control-progress-time">
+                                <div className="player__control-time_remaining">
+                                    13:26
+                                </div>
+                            </div>
                         </div>
                         <div className="player__control-buttons">
                             <button className="player__control-button">
-                                <i className="fa fa-play player__control-button-icon" aria-hidden="true"></i>
+                                <i className="fa fa-pause player__control-button-icon" aria-hidden="true"></i>
                             </button>
                             <button className="player__control-button">
                                 <i className="fa fa-backward player__control-button-icon" aria-hidden="true"></i>
@@ -35,6 +57,15 @@ class Player extends React.Component<PlayerProps, PlayerState> {
                             <div className="player__control-title">
                                 La La Land
                             </div>
+                            <button className="player__control-button">
+                                <i className="fa fa-question-circle-o player__control-button-icon" aria-hidden="true"></i>
+                            </button>
+                            <button className="player__control-button">
+                                <i className="fa fa-cc player__control-button-icon" aria-hidden="true"></i>
+                            </button>
+                            <button className="player__control-button">
+                                <i className="fa fa-expand player__control-button-icon" aria-hidden="true"></i>
+                            </button>
                         </div>
                     </div>
                 </div>
